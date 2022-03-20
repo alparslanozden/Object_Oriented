7a9227ae699ed336863744beca875d1bde22140f
origin <https://github.com/alparslanozden/Kotlin_Notes>
2022-03-20 20:32:04 +0300

    Add viewBinding dependency

--- app/src/main/java/com/alparslan/object_oriented/MainActivity.kt
+++ app/src/main/java/com/alparslan/object_oriented/MainActivity.kt
@@ -2,10 +2,17 @@ package com.alparslan.object_oriented
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alparslan.object_oriented.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
